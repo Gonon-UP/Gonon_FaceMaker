728d40de0d51d640b70dcc28a728553598c5f43e
Gonon-UP <gonon24@up.edu>
2024-02-22 10:28:55 -0800

    Updated Face Class

--- app/src/main/java/com/example/gonon_facemaker/Face.java
+++ app/src/main/java/com/example/gonon_facemaker/Face.java
@@ -4,8 +4,13 @@
 
 package com.example.gonon_facemaker;
 import java.util.Random;
+
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
 
 /*
  *
@@ -13,7 +18,7 @@ import android.graphics.Color;
  * Basic class to describe what a face is and it's characteristics
  *
  */
-public class Face {
+public class Face extends SurfaceView {
 
     // Initializing properties
     private int skinColor;
@@ -21,9 +26,6 @@ public class Face {
     private int hairColor;
     private int hairStyle; // Identifies which hair style the face has
 
-    public Face()   {
-        randomize();
-    }
 
 
     // randomize method will randomize all four int properties for face
@@ -31,22 +33,118 @@ public class Face {
         Random random = new Random();
 
         // Randomizing int values for property values
-        this.skinColor = Color.argb(255, 55, 55, 55);
-        this.eyeColor = random.nextInt(256);
-        this.hairColor = random.nextInt(256);
+        this.skinColor = Color.argb(255, random.nextInt(255),
+                random.nextInt(255), random.nextInt(255));
+
+        this.eyeColor = Color.argb(255, random.nextInt(255),
+                random.nextInt(255), random.nextInt(255));
+
+        this.hairColor = Color.argb(255, random.nextInt(255),
+                random.nextInt(255), random.nextInt(255));
+
         this.hairStyle = random.nextInt(4); // Total of 4 hairstyle options
     }
 
 
-    public void onDraw(Canvas canvas)   {
-        // To be implemented soon
-//        drawFace(canvas, yellow);
-//        drawEyes(canvas, black);
-//        drawHair(canvas, black, 1);
+    //It's a good habit to create Paint objects just once rather than
+    //recreate them each time onDraw() is called
+    private final Paint black = new Paint();
+    private final Paint yellow = new Paint();
+    private float width;
+
+    // Preliminary stages for SurfaceView
+    public Face(Context context) {
+        super(context);
+    }
+
+    public Face(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public Face(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
 
-    // Getter methods for instance variables
+    public Face(Context context, AttributeSet attrs)    {
+
+        super(context, attrs);
+
+        //hex colors
+        black.setColor(0xFF000000);
+        yellow.setColor(0xFFFFFF00);
+        Paint white = new Paint();
+        white.setColor(0xFFFFFFFF);
+
+        this.setWillNotDraw(false);
+    }
+
+    public void drawFace(Canvas canvas, Paint Color) // Location on canvas
+    {
+        // For face
+        canvas.drawCircle((width / 2), 350, 300,
+                Color);
+
+        // Mouth
+        canvas.drawOval((width / 2) - 180, 340,(width / 2) + 180,
+                540, black);
+
+        // Smile
+        canvas.drawRect((width / 2) - 180, 410,
+                (width / 2) + 180, 440, black);
+        canvas.drawOval((width / 2) - 180, 339,
+                (width / 2) + 180, 480, Color);
+    }
+
+    public void drawEyes(Canvas canvas, Paint Color)    {
+        // For eyes
+        canvas.drawCircle((width / 2) - 130, 250, 45,  Color); // Left eye
+        canvas.drawCircle((width / 2) + 130, 250, 45, Color); // Right eye
+    }
+
+    public void drawHair(Canvas canvas, Paint Color, int option)   {
+        int i;
+        if (option == 0)    {
+            // Draw nothing, bald
+            //canvas.drawLine((width / 2) - 200, 150, );
+            canvas.drawPoint((width / 2) - 250, 150, black);
+            canvas.drawPoint((width / 2) + 250, 150, black);
+            canvas.drawPoint((width / 2), 50, black);
+        }
+        else if (option == 1) {
+            for (i = 0; i < 400; i += 20) {
+                canvas.drawLine((width / 2) - 200 + i, 50,
+                        (width / 2) - 200 + i, 150, Color);
+            }
+        }
+        else if (option == 2)   {
+            // Draw Curly Hair
+        }
+        else if (option == 3)   {
+            // draw
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        drawFace(canvas, yellow);
+        drawEyes(canvas, black);
+        drawHair(canvas, black, 0);
+    }
+
+
+
+
+    // Retrieves dimensions of the layout width
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        width = MeasureSpec.getSize(widthMeasureSpec);
+    }
+
+
+
+// Getter methods for instance variables
     public int getSkinColor() {
         return this.skinColor;
     }
